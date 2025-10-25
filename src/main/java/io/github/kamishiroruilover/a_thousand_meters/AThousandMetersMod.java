7ef972fd6f5db89927b12e187cb8c41d15ca61fc
Rui <iplayfo4@gmail.com>
2025-10-25 10:46:33 -0500

    Customized Template

--- src/main/java/io/github/kamishiroruilover/a_thousand_meters/AThousandMetersMod.java
+++ src/main/java/io/github/kamishiroruilover/a_thousand_meters/AThousandMetersMod.java
@@ -1,11 +1,11 @@
-package com.example.example_mod;
+package io.github.kamishiroruilover.a_thousand_meters;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class AThousandMetersMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
